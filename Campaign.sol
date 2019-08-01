b8801f92b1ef19a46ba00356b36af3ad2d9d5fde
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-02 00:16:37 +0530

    Campaign : collection of key value pairs struct Request defination created

--- Campaign.sol
+++ Campaign.sol
@@ -1,10 +1,21 @@
 pragma solidity ^0.4.17;
 
 contract Campaign{
+    // collection of key value pairs
+    struct Request{
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+    }
+    
+    // === Fields ===
     address public manager;
     uint public minimumContribution;
     address[] public approvers;
     
+    
+    // === Methods ===
     //Setting the manager and minimum amount to contribute
     function Compaign(uint minimum) public {
         manager = msg.sender;
