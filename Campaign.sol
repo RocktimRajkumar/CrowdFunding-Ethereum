4fa9ee848e8e5047dd225edf55a9e64a144b4acf
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-01 23:56:47 +0530

    Campaign : contribute function done

--- Campaign.sol
+++ Campaign.sol
@@ -3,9 +3,17 @@ pragma solidity ^0.4.17;
 contract Campaign{
     address public manager;
     uint public minimumContribution;
+    address[] public approvers;
     
+    //Setting the manager and minimum amount to contribute
     function Compaign(uint minimum) public {
         manager = msg.sender;
         minimumContribution = minimum;
+    }
+    
+    //donate money to compaign and became an approver
+    function contribute() public payable{
+        require(msg.value > minimumContribution);
+        approvers.push(msg.sender);
     }
 }
