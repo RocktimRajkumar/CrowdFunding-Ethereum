e78773434ea31265c5f740a446abfdc6ed98090d
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-10 18:24:02 +0530

    test > Campaign.test.js : case deploys a factory and Campaigns done

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -16,17 +16,24 @@ beforeEach(async () => {
 
   factory = await new web3.eth.Contract(JSON.parse(compileFactory.interface))
     .deploy({ data: compileFactory.bytecode })
-    .send({ from: accounts[0], gas: '1000000' });
+    .send({ from: accounts[0], gas: "1000000" });
 
-  await factory.methods.createCampaign('100').send({
-      from : accounts[0],
-      gas : '100000'
+  await factory.methods.createCampaign("100").send({
+    from: accounts[0],
+    gas: "1000000"
   });
 
   [campaignAddress] = await factory.methods.getDeployedCampaigns().call(); // [campaignAddress] =>  campaignAddress = address[0]
-  
+
   campaign = await new web3.eth.Contract(
-      JSON.parse(compileCampaign.interface),
-      campaignAddress
-      );
+    JSON.parse(compileCampaign.interface),
+    campaignAddress
+  );
+});
+
+describe("Campaigns", () => {
+  it("deploys a factory and Campaigns", () => {
+    assert.ok(factory.options.address);
+    assert.ok(campaign.options.address);
+  });
 });
