426304d35df3c4b82662c62f25219afee873f8c3
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-10 17:12:30 +0530

    test > Campaign.test.js : beforeEach mocha test done

---
+++ test/Campaign.test.js
@@ -0,0 +1,32 @@
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
+const web3 = new Web3(ganache.provider());
+
+const compileFactory = require("../Ethereum/build/CampaignFactory.json");
+const compileCampaign = require("../Ethereum/build/Campaign.json");
+
+let accounts;
+let factory;
+let campaignAddress;
+let campaign;
+
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+
+  factory = await new web3.eth.Contract(JSON.parse(compileFactory.interface))
+    .deploy({ data: compileFactory.bytecode })
+    .send({ from: accounts[0], gas: '1000000' });
+
+  await factory.methods.createCampaign('100').send({
+      from : accounts[0],
+      gas : '100000'
+  });
+
+  [campaignAddress] = await factory.methods.getDeployedCampaigns().call(); // [campaignAddress] =>  campaignAddress = address[0]
+  
+  campaign = await new web3.eth.Contract(
+      JSON.parse(compileCampaign.interface),
+      campaignAddress
+      );
+});
