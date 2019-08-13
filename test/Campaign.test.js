f29a7698e60a55cf8f64496b9f45cd6f34d84119
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-13 22:03:13 +0530

    test > Campaign.test.js : test case - marks caller as the campaign manager

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -1,10 +1,10 @@
-const assert = require("assert");
-const ganache = require("ganache-cli");
-const Web3 = require("web3");
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
-const compileFactory = require("../Ethereum/build/CampaignFactory.json");
-const compileCampaign = require("../Ethereum/build/Campaign.json");
+const compileFactory = require('../Ethereum/build/CampaignFactory.json');
+const compileCampaign = require('../Ethereum/build/Campaign.json');
 
 let accounts;
 let factory;
@@ -16,11 +16,11 @@ beforeEach(async () => {
 
   factory = await new web3.eth.Contract(JSON.parse(compileFactory.interface))
     .deploy({ data: compileFactory.bytecode })
-    .send({ from: accounts[0], gas: "1000000" });
+    .send({ from: accounts[0], gas: '1000000' });
 
-  await factory.methods.createCampaign("100").send({
+  await factory.methods.createCampaign('100').send({
     from: accounts[0],
-    gas: "1000000"
+    gas: '1000000'
   });
 
   [campaignAddress] = await factory.methods.getDeployedCampaigns().call(); // [campaignAddress] =>  campaignAddress = address[0]
@@ -31,9 +31,15 @@ beforeEach(async () => {
   );
 });
 
-describe("Campaigns", () => {
-  it("deploys a factory and Campaigns", () => {
+describe('Campaigns', () => {
+  it('deploys a factory and Campaigns', () => {
     assert.ok(factory.options.address);
     assert.ok(campaign.options.address);
   });
+
+  it('marks caller as the campaign manager', async () => {
+    const manager = await campaign.methods.manager().call();
+    assert.equal(accounts[0], manager);
+  })
+
 });
