8a385b13fc4cddf239469ad2e5bc42bcc638b8ba
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-15 20:36:58 +0530

    Ethereum > web3.js : web3 provider fix running in server and client and without meta mask

--- Ethereum/web3.js
+++ Ethereum/web3.js
@@ -1,6 +1,18 @@
 import Web3 from 'web3';
 
-const web3 = new Web3(window.web3.currentProvider);
+let web3;
 
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+    // we are in the browser and meta mask is installed
+    web3 = new Web3(window.web3.currentProvider);
+} else {
+    // we are on the server *OR* meta mask is not running
+    // creating our own provider
+    const provider = new Web3.providers.HttpProvider(
+        process.env.link
+    );
+
+    web3 = new Web3(provider);
+}
 
 export default web3;
