3670c8a92a4b2d478b6a1a932263bda03ada31b0
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-16 11:16:16 +0530

    pages > index.js : Card.Group added

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import factory from '../Ethereum/factory';
+import { Card } from 'semantic-ui-react';
 
 class CampaignIndex extends Component {
 
@@ -9,8 +10,23 @@ class CampaignIndex extends Component {
         return { campaigns };
     }
 
+    renderCampaigns() {
+        const items = this.props.campaigns.map(address => {
+            return {
+                header: address,
+                description: <a>View Campaign</a>,
+                fluid: true,
+            };
+        });
+
+        return <Card.Group items={items} />;
+    }
+
     render() {
-        return <div>{this.props.campaigns[0]}</div>;
+        return <div>
+        <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
+        {this.renderCampaigns()}
+        </div>;
     }
 }
 
