d6de79718dc29a0c0d067794e376f5b10bac8fec
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-17 20:19:06 +0530

    components > Layout.js : importing header content

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import Header from './Header';
 
 export default props => {
     return (
         <div>
-            <h1>I am a header</h1>
+            <Header />
             {props.children}
-            <h1>I am a footer</h1>
         </div>
     );
 };
