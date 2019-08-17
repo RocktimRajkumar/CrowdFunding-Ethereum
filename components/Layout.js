e454df71b4242b417c0d32cbbcfb869f0c73cf59
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-17 20:23:55 +0530

    components > Layout.js : margin added in left and right

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from './Header';
+import { Container } from 'semantic-ui-react';
 
 export default props => {
     return (
-        <div>
+        <Container>
             <Header />
             {props.children}
-        </div>
+        </Container>
     );
 };
