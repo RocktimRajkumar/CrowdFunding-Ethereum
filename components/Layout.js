0390aeb057aa89e316f658a336798913906ad790
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-17 23:10:50 +0530

    components > Layout.js : stylesheet link tag moved from index.js

--- components/Layout.js
+++ components/Layout.js
@@ -5,6 +5,7 @@ import { Container } from 'semantic-ui-react';
 export default props => {
     return (
         <Container>
+            <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
             <Header />
             {props.children}
         </Container>
