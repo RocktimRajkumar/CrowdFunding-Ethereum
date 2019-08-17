e2e157a66da9e38031e36f2682fe7c1d5211b641
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-17 19:41:14 +0530

    pages > index.js : label icon button create campaign added

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import factory from '../Ethereum/factory';
-import { Card } from 'semantic-ui-react';
+import { Card, Button } from 'semantic-ui-react';
 
 class CampaignIndex extends Component {
 
@@ -26,6 +26,12 @@ class CampaignIndex extends Component {
         return <div>
         <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
         {this.renderCampaigns()}
+        <Button 
+            content="Create Campaign"
+            icon = "add circle"
+            primary
+            
+        />
         </div>;
     }
 }
