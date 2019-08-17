03e2392a8245a131e8c82a443de7615313c786b8
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-17 19:49:43 +0530

    pages > index.js : heading added

--- pages/index.js
+++ pages/index.js
@@ -25,12 +25,13 @@ class CampaignIndex extends Component {
     render() {
         return <div>
         <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
+        <h3>Open Campaigns</h3>
         {this.renderCampaigns()}
         <Button 
             content="Create Campaign"
             icon = "add circle"
             primary
-            
+
         />
         </div>;
     }
