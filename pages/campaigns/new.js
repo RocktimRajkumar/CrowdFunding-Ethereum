cfdf98a1783c587ddd8460f5ee8083ec53343fc5
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-18 13:13:24 +0530

    pages > campaigns > new.js : createCampaign exception handling done and showing error

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
-import { Form, Button, Input } from 'semantic-ui-react';
+import { Form, Button, Input, Message } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import factory from '../../Ethereum/factory';
 import web3 from '../../Ethereum/web3';
 
 export default class CampaignNew extends Component {
     state = {
-        minimumContribution: ''
+        minimumContribution: '',
+        errorMessage: ''
     };
 
     onSubmit = async (event) => {
         event.preventDefault(); // keep the browser to automatically submit the form to the server
 
-        const accounts = await web3.eth.getAccounts();
-        await factory.methods.createCampaign(this.state.minimumContribution)
-            .send({
-                from: accounts[0]
-            });
+        try {
+            const accounts = await web3.eth.getAccounts();
+            await factory.methods.createCampaign(this.state.minimumContribution)
+                .send({
+                    from: accounts[0]
+                });
+        } catch (err) {
+            this.setState({ errorMessage: err.message });
+        }
     };
 
     render() {
@@ -24,7 +29,7 @@ export default class CampaignNew extends Component {
             <Layout>
                 <h3>Create a Campaign</h3>
 
-                <Form onSubmit={this.onSubmit}>
+                <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
                     <Form.Field>
                         <label>Minimum Contribution</label>
                         <Input
@@ -37,6 +42,7 @@ export default class CampaignNew extends Component {
                         />
                     </Form.Field>
 
+                    <Message error header="Oops!" content={this.state.errorMessage} />
                     <Button type="submit" primary>Create</Button>
                 </Form>
             </Layout>
