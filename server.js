6cce98ef9961b5a69e4fc4bd1e3a9f982ef351af
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-18 14:38:55 +0530

    server.js : file added for boot up next app and tell it to use routes.js

---
+++ server.js
@@ -0,0 +1,18 @@
+//  Boot up 'next' app, tell it to use routes.js
+
+const { createServer } = require('http');
+const next = require('next');
+
+const app = next({
+    dev: process.env.NODE_ENV !== 'production'
+});
+
+const routes = require('./routes');
+const handler = routes.getRequestHandler(app);
+
+app.prepare().then(() => {
+    createServer(handler).listen(3000, (err) => {
+        if (err) throw err;
+        console.log('Ready on localhost: 3000');
+    });
+});
