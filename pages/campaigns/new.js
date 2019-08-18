a525c45ff1fd2552ce14875aece84950fd9ccc37
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-18 12:12:00 +0530

    pages > campaigns > new.js : minimumContribution label and state added

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
-import { Form, Button } from 'semantic-ui-react';
+import { Form, Button, Input } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 
 export default class CampaignNew extends Component {
+    state = {
+        minimumContribution: ''
+    };
+
     render() {
         return (
             <Layout>
@@ -11,7 +15,14 @@ export default class CampaignNew extends Component {
                 <Form>
                     <Form.Field>
                         <label>Minimum Contribution</label>
-                        <input />
+                        <Input
+                            label="wei"
+                            labelPosition="right"
+                            value={this.state.minimumContribution}
+                            onChange={event => {
+                                this.setState({ minimumContribution: event.target.value })
+                            }}
+                        />
                     </Form.Field>
 
                     <Button type="submit" primary>Create</Button>
