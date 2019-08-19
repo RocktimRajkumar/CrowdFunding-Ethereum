253bc7aa59c2107900434a6bc14dde1396542f6b
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-19 22:09:46 +0530

    Ethereum > factory.js : contract address replace

--- Ethereum/factory.js
+++ Ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
     JSON.parse(CampaignFactory.interface),
-    '0x77c20AD7a302198281608F577648026a2DE78d26'
+    '0xEa61FAeED730d6aCF55e15Ecf50dcA53ddE0D1e7'
 );
 
 export default instance;
