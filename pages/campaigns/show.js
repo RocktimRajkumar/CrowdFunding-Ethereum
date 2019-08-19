1635d0b98aa65b3c10f311f593969545f46c9eea
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-19 22:48:33 +0530

    pages > campaigns > show.js : getSummary method called

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
+import Campaign from '../../Ethereum/campaign';
 
 export default class CampaignShow extends Component {
+    static async getInitialProps(props) {
+        const campaign = Campaign(props.query.address);
+        console.log(await campaign.methods.getSummary().call());
+        return {};
+    }
+
     render() {
         return (
             <Layout>
