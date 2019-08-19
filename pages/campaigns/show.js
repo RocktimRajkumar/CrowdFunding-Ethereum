a801c817dedd93cb4d5acd46dcc92eddf5c0e9de
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-19 20:35:16 +0530

    pages > campaigns > show.js : file added to show campaign details

---
+++ pages/campaigns/show.js
@@ -0,0 +1,9 @@
+import React , {Component} from 'react';
+
+export default class CampaignShow extends Component{
+    render(){
+        return (
+            <h3>Campaign Show</h3>
+        );
+    }
+}
