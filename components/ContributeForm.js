faaae005a0e1be9ba4a3a76849ff9367e703cd8d
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-20 08:39:59 +0530

    components > ContributeForm.js : state and onSubmit added

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import { Form, Input, Message, Button } from 'semantic-ui-react';
+import Campaign from '../Ethereum/campaign';
 
 
 export default class ContributeForm extends Component {
+    state = {
+        value: ''
+    };
+
+    onSubmit = (event) => {
+        event.preventDefault();
+
+        const campaign = Campaign(this.props.address);
+    };
+
     render() {
         return (
-            <Form>
+            <Form onSubmit={this.onSubmit}>
                 <Form.Field>
                     <label>Amount to Contribute</label>
                     <Input
+                        value={this.state.value}
                         label="ether"
                         labelPosition="right"
+                        onChange={event = this.setState({
+                            value: event.target.value
+                        })}
                     />
                 </Form.Field>
                 <Button primary>
