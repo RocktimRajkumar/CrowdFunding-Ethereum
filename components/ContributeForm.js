e99d77432475a8b8536523164b7ec7e422fcc69c
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-20 08:53:50 +0530

    components > ContributeForm.js : contribute to campaign done

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Form, Input, Message, Button } from 'semantic-ui-react';
 import Campaign from '../Ethereum/campaign';
+import web3 from '../Ethereum/web3';
 
 
 export default class ContributeForm extends Component {
@@ -8,10 +9,20 @@ export default class ContributeForm extends Component {
         value: ''
     };
 
-    onSubmit = (event) => {
+    onSubmit = async (event) => {
         event.preventDefault();
 
         const campaign = Campaign(this.props.address);
+
+        try {
+            const accounts = await web3.eth.getAccounts();
+            await campaign.methods.contribute().send({
+                from: accounts[0],
+                value: web3.utils.toWei(this.state.value, 'ether')
+            });
+        } catch (error) {
+
+        }
     };
 
     render() {
@@ -23,7 +34,7 @@ export default class ContributeForm extends Component {
                         value={this.state.value}
                         label="ether"
                         labelPosition="right"
-                        onChange={event = this.setState({
+                        onChange={event => this.setState({
                             value: event.target.value
                         })}
                     />
