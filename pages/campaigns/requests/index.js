6e8c58c381da81b754464de1737af2080d888e05
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-21 23:33:51 +0530

    pages > campaigns > requests  : directory for campaign request created

---
+++ pages/campaigns/requests/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class RequestIndex extends Component {
+    render() {
+        return (
+            <h3>Request List</h3>
+        )
+    }
+}
