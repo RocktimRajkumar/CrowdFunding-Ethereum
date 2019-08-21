16571f36d834dcf6557a90f31cec9b26aa73e6f8
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-21 23:58:58 +0530

    pages > campaigns > requests > index.js : redirecting to new campaign request added

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,9 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
+import Layout from '../../../components/Layout';
+import { Link } from '../../../routes';
 
 export default class RequestIndex extends Component {
+
+    static async getInitialProps(props) {
+        const { address } = props.query;
+        return { address };
+    }
+
     render() {
         return (
-            <h3>Request List</h3>
+            <Layout>
+                <h3>Request</h3>
+                <Link route={`/campaigns/${this.props.address}/requests/new`}>
+                    <a>
+                        <Button primary>Add Request</Button>
+                    </a>
+                </Link>
+            </Layout>
         )
     }
 }
