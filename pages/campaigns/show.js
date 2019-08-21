6c677e65b84993f88fdc5b40c7ff6b9d93ca1878
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-21 23:43:50 +0530

    pages > campaigns > show.js : Grid Row added

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -70,17 +70,30 @@ export default class CampaignShow extends Component {
             <Layout>
                 <h3>Campaign Show</h3>
                 <Grid>
-                    <Grid.Column width={10}>
-                        {this.renderCard()}
-                        <Link route={`/campaigns/${this.props.address}/requests`}>
-                            <a>
-                                <Button primary>View Requests</Button>
-                            </a>
-                        </Link>
-                    </Grid.Column>
-                    <Grid.Column width={6}>
-                        <ContributeForm address={this.props.address} />
-                    </Grid.Column>
+                    <Grid.Row>
+
+                        <Grid.Column width={10}>
+                            {this.renderCard()}
+
+                        </Grid.Column>
+
+                        <Grid.Column width={6}>
+                            <ContributeForm address={this.props.address} />
+                        </Grid.Column>
+
+                    </Grid.Row>
+
+                    <Grid.Row>
+
+                        <Grid.Column>
+                            <Link route={`/campaigns/${this.props.address}/requests`}>
+                                <a>
+                                    <Button primary>View Requests</Button>
+                                </a>
+                            </Link>
+                        </Grid.Column>
+
+                    </Grid.Row>
                 </Grid>
             </Layout>
         );
