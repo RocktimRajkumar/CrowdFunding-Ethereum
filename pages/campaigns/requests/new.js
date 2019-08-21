6d4200f445bfdd9e14fa98fb47fd91d9ea2db83d
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-21 23:58:05 +0530

    pages > campaigns > requests > new.js : page for new campaign request added

---
+++ pages/campaigns/requests/new.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class RequestNew extends Component {
+    render() {
+        return (
+            <h3>Create a Request</h3>
+        )
+    }
+}
