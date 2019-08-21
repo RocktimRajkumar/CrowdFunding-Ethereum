6042f75cd88f1e8b09322b61d20e4d2a5d3426f9
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-21 23:32:21 +0530

    routes.js : routes mapping for campaigns address added

--- routes.js
+++ routes.js
@@ -4,6 +4,7 @@ const routes = require('next-routes')(); //return a function and () means execut
 
 routes
     .add('/campaigns/new', '/campaigns/new')
-    .add('/campaigns/:address', '/campaigns/show');
+    .add('/campaigns/:address', '/campaigns/show')
+    .add('/campaigns/:address/requests', '/campaigns/requests/index');
 
 module.exports = routes;
