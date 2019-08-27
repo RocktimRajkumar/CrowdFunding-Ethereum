a17d741cf9a59cd5beb67a6c693a9b772593f35c
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-27 08:45:10 +0530

    pages > campaigns > requests > index.js : semantic-ui table added

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react';
+import { Button, Table } from 'semantic-ui-react';
 import Layout from '../../../components/Layout';
 import { Link } from '../../../routes';
 import Campaings from '../../../Ethereum/campaign';
@@ -21,6 +21,9 @@ export default class RequestIndex extends Component {
     }
 
     render() {
+
+        const { Header, Row, HeaderCell, Body } = Table;
+
         return (
             <Layout>
                 <h3>Request</h3>
@@ -29,6 +32,21 @@ export default class RequestIndex extends Component {
                         <Button primary>Add Request</Button>
                     </a>
                 </Link>
+
+                <Table>
+                    <Header>
+                        <Row>
+                            <HeaderCell>ID</HeaderCell>
+                            <HeaderCell>Description</HeaderCell>
+                            <HeaderCell>Amount</HeaderCell>
+                            <HeaderCell>Recipient</HeaderCell>
+                            <HeaderCell>Approval</HeaderCell>
+                            <HeaderCell>Approve</HeaderCell>
+                            <HeaderCell>Finalize</HeaderCell>
+                        </Row>
+                    </Header>
+                </Table>
+
             </Layout>
         )
     }
