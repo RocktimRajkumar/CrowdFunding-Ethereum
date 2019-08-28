7b1d4e6d392353d15e0cee9d18d5a07f8daf9e00
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-28 08:56:58 +0530

    componenets/RequestRow.js : button click approve request done

--- components/RequestRow.js
+++ components/RequestRow.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react'
 import { Table, Button } from 'semantic-ui-react';
 import web3 from '../Ethereum/web3';
+import Campaign from '../Ethereum/campaign';
 
 export default class RequestRow extends Component {
+
+    onApprove = async () => {
+        const campaign = Campaign(this.props.address);
+
+        const accounts = await web3.eth.getAccounts();
+        await campaign.methods.approveRequest(this.props.id).send({
+            from: accounts[0]
+        });
+    };
+
     render() {
         const { Row, Cell } = Table;
         const { id, request, approversCount } = this.props;
@@ -14,7 +25,7 @@ export default class RequestRow extends Component {
                 <Cell>{request.recipient}</Cell>
                 <Cell>{request.approvalCount}/{approversCount}</Cell>
                 <Cell>
-                <Button color='green' basic>Approve</Button>
+                    <Button color='green' basic onClick={this.onApprove}>Approve</Button>
                 </Cell>
                 <Cell>{}</Cell>
             </Row>
